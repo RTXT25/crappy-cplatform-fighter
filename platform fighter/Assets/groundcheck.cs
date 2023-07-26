951771d589cb292ea76a5bb92ed1ab62bf77c204
Nvidia RTXT2500 <78957603+RTXT25@users.noreply.github.com>
2023-07-26 13:00:36 +1000

    not pain

--- platform fighter/Assets/groundcheck.cs
+++ platform fighter/Assets/groundcheck.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class groundcheck : MonoBehaviour{
     void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.CompareTag("solidplat") || other.gameObject.CompareTag("semisolid")){
-            move.jumpsLeft = move.maxJumps;
+            move.isGround = true;
+        }   
+    }
+    void OnTriggerExit2D(Collider2D other) {
+        if(other.gameObject.CompareTag("solidplat") || other.gameObject.CompareTag("semisolid")){
+            move.isGround = false;
         }   
     }
 }
--- platform fighter/Assets/move.cs
+++ platform fighter/Assets/move.cs
@@ -2,36 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class move : MonoBehaviour{    
+public class move : MonoBehaviour{
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
+    public float jumpHoldForce = 2.5f;
+    public float normalFall = 5f;
     public float fallMult = 2.5f;
     static public int maxJumps = 2;
-    Rigidbody2D rb;
+    CharacterController controller;
     bool inFastFall;
     static public bool isGround;
     static public int jumpsLeft;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    public float groundDistance = 0.2f;
+
+    // Jump-related variables
+    bool isJumping = false;
+    float verticalVelocity = 0f;
+
     void Awake(){
-        rb = GetComponent<Rigidbody2D>();
+        controller = GetComponent<CharacterController>();
     }
     void Start(){
         jumpsLeft = maxJumps;
     }
     void Update(){
-        float moveX = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
-        if (Input.GetButtonDown("Jump")){
-            if (jumpsLeft > 0){
-                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-                jumpsLeft--;
-            }
+        isGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGround && verticalVelocity < 0){
+            jumpsLeft = maxJumps;
+            verticalVelocity = -2f; 
         }
-        if (Input.GetAxisRaw("Vertical") < 0){
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMult - 1) * Time.deltaTime;
-            if(isGround){
-                jumpsLeft = maxJumps;
-                //Input.GetAxis("Vertical") = 1;
-            }
+
+       if (Input.GetKeyDown(KeyCode.Space) && jumpsLeft > 0){
+            verticalVelocity = jumpForce;
+            isJumping = true;
+            jumpsLeft--;
         }
+        if (isJumping && Input.GetKey(KeyCode.Space)){
+            verticalVelocity += jumpHoldForce * Time.deltaTime;
+        }
+        if (!isGround) {
+            verticalVelocity -= normalFall * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow)){
+            verticalVelocity -= fallMult * normalFall * Time.deltaTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float wantX = Input.GetAxisRaw("Horizontal") * Time.fixedDeltaTime * moveSpeed;
+        float wantY = verticalVelocity * Time.fixedDeltaTime;
+
+        Vector3 moveDirection = new Vector3(wantX, wantY, 0);
+        controller.Move(moveDirection);
     }
 }
